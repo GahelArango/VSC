ba74f75d31bcb8212fc34baa04012a8990abc542
GahelArango <garangososa@guilford.edu>
2024-01-10 14:07:31 -0500

    Updated print to include classname

--- demoo/src/main/java/edu/guilforddd/Sandbox.java
+++ demoo/src/main/java/edu/guilforddd/Sandbox.java
@@ -2,6 +2,6 @@ package edu.guilforddd;
 
 public class Sandbox {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world from CTIS 310!");
     }
 }
